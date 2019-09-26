2c80f3ea14390b5adda79051290d2e389e1a8566
duylan <duylan426@gmail.com>
2019-09-26 15:27:56 +0700

    test1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
+    
     {
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+
+        }
+
+        public static bool IsOdd(int n)
+        {
+            bool dk = false;
+            if ( n % 2 ! = 0);
+            {
+                dk = true;
+            }
+
+            return dk;
+        }
+
+        public static bool IsEven(int n)
+        {
+             bool dk = false;
+            if ( n % 2 ! = 0);
+            {
+                dk = true;
+            }
+            return dk;
         }
     }
 }
